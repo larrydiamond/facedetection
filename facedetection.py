626229616d537fc5cdb0885397be8839aeeefadc
Larry Diamond <1066589+larrydiamond@users.noreply.github.com>
2024-01-06 20:35:25 -0500

    removed imagedraw

--- facedetection.py
+++ facedetection.py
@@ -37,26 +37,6 @@ def _recognize_face(unknown_encoding, loaded_encodings):
 
     return max(matches.items(), key=operator.itemgetter(1))[0]
 
-def _display_face(draw, bounding_box, name):
-    """
-    Draws bounding boxes around faces, a caption area, and text captions.
-    """
-    top, right, bottom, left = bounding_box
-    draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR)
-    text_left, text_top, text_right, text_bottom = draw.textbbox(
-        (left, bottom), name
-    )
-    draw.rectangle(
-        ((text_left, text_top), (text_right, text_bottom)),
-        fill=BOUNDING_BOX_COLOR,
-        outline=BOUNDING_BOX_COLOR,
-    )
-    draw.text(
-        (text_left, text_top),
-        name,
-        fill=TEXT_COLOR,
-    )
-
 # Load labeled images
 print ("Loading known images")
 
@@ -90,7 +70,6 @@ for bounding_box, unknown_encoding in zip(
     name = _recognize_face(unknown_encoding, name_encodings)
     if not name:
         name = "Unknown"
-    _display_face(draw, bounding_box, name)
     print (name)
 
 
